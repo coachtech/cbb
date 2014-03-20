074e864e79ae7a2c941e7b603a91d58beb917568
coachtech <coachtech@gmail.com>
2014-03-20 10:42:08 -0700

    Simpler and More Visually Pleasing
    Think it is pretty much ready to go

--- Set_Draft_Lottery_Working.py
+++ Set_Draft_Lottery_Working.py
@@ -29,7 +29,7 @@ team9 = [input('Which team finished in 9th place? '),False, team_9]
 team10 = [input('Which team finished in 10th place? '),False, team_10]
 team11 = [input('Which team finished in 11th place? '),False, team_11]
 team12 = [input('Which team finished in 12th place? '),False, team_12]
-
+print()
 allteams = [team1, team2, team3, team4, team5, team6, team7, team8,team9,team10,team11,team12]
 
 ### adding new set items to all_teams
@@ -62,158 +62,164 @@ populate(team_11, 11)
 populate(team_12, 12)
 
 
-print (team_1)
-print (team_2)
-print (team_3)
-print (team_4)
-print (team_5)
-print (team_6)
-print (team_7)
-print (team_8)
-print (team_9)
-print (team_10)
-print (team_11)
-print (team_12)
-
+print (team1[0]+ " has the following balls: " + str(team_1))
+time.sleep(1)
+print (team2[0]+ " has the following balls: " + str(team_2))
+time.sleep(1)
+print (team3[0]+ " has the following balls: " + str(team_3))
+time.sleep(1)
+print (team4[0]+ " has the following balls: " + str(team_4))
+time.sleep(1)
+print (team5[0]+ " has the following balls: " + str(team_5))
+time.sleep(1)
+print (team6[0]+ " has the following balls: " + str(team_6))
+time.sleep(1)
+print (team7[0]+ " has the following balls: " + str(team_7))
+time.sleep(1)
+print (team8[0]+ " has the following balls: " + str(team_8))
+time.sleep(1)
+print (team9[0]+ " has the following balls: " + str(team_9))
+time.sleep(1)
+print (team10[0]+ " has the following balls: " + str(team_10))
+time.sleep(1)
+print (team11[0]+ " has the following balls: " + str(team_11))
+time.sleep(1)
+print (team12[0]+ " has the following balls: " + str(team_12))
+print()
 ### Picking Random #s from All Teams
 ### 
 
 ## first pick
 firstPick = random.sample(all_teams, 1)
 p1 = firstPick[0]
-print (p1)
 
 if p1  in team_1:
     all_teams.difference_update(team_1)
-    x =  ("The first pick goes to ", team1[0])
+    x =  ("The first pick goes to "+ team1[0])
 elif p1 in team_2:
     all_teams.difference_update(team_2)
-    x=  ("The first pick goes to ", team2[0])
+    x=  ("The first pick goes to "+ team2[0])
 elif p1 in team_3:
     all_teams.difference_update(team_3)
-    x=  ("The first pick goes to ", team3[0])
+    x=  ("The first pick goes to "+ team3[0])
 elif p1 in team_4:
     all_teams.difference_update(team_4)
-    x=  ("The first pick goes to ", team4[0])
+    x=  ("The first pick goes to "+ team4[0])
 elif p1 in team_5:
     all_teams.difference_update(team_5)
-    x=  ("The first pick goes to ", team5[0])
+    x=  ("The first pick goes to "+ team5[0])
 elif p1 in team_6:
     all_teams.difference_update(team_6)
-    x=  ("The first pick goes to ", team6[0])
+    x=  ("The first pick goes to "+ team6[0])
 elif p1 in team_7:
     all_teams.difference_update(team_7)
-    x=  ("The first pick goes to ", team7[0])
+    x=  ("The first pick goes to "+ team7[0])
 elif p1 in team_8:
     all_teams.difference_update(team_8)
-    x=  ("The first pick goes to ", team8[0])
+    x=  ("The first pick goes to "+ team8[0])
 elif p1 in team_9:
     all_teams.difference_update(team_9)
-    x=  ("The first pick goes to ", team9[0])
+    x=  ("The first pick goes to "+ team9[0])
 elif p1 in team_10:
     all_teams.difference_update(team_10)
-    x=  ("The first pick goes to ", team10[0])
+    x=  ("The first pick goes to "+ team10[0])
 elif p1 in team_11:
     all_teams.difference_update(team_11)
-    x=  ("The first pick goes to ", team11[0])
+    x=  ("The first pick goes to "+ team11[0])
 else:
     all_teams.difference_update(team_12)
-    x =  ("The first pick goes to ", team12[0])
+    x =  ("The first pick goes to "+ team12[0])
 
-print (all_teams)
 
 ## second pick
 secondPick = random.sample(all_teams, 1)
 p2 = secondPick[0]
-print (p2)
 
 if p2  in team_1:
     all_teams.difference_update(team_1)
-    y =  ("The second pick goes to ", team1[0])
+    y =  ("The second pick goes to " + team1[0])
 elif p2 in team_2:
     all_teams.difference_update(team_2)
-    y=  ("The second  pick goes to ", team2[0])
+    y=  ("The second  pick goes to "+ team2[0])
 elif p2 in team_3:
     all_teams.difference_update(team_3)
-    y=  ("The second pick goes to ", team3[0])
+    y=  ("The second pick goes to "+ team3[0])
 elif p2 in team_4:
     all_teams.difference_update(team_4)
-    y=  ("The second pick goes to ", team4[0])
+    y=  ("The second pick goes to "+ team4[0])
 elif p2 in team_5:
     all_teams.difference_update(team_5)
-    y=  ("The second pick goes to ", team5[0])
+    y=  ("The second pick goes to "+ team5[0])
 elif p2 in team_6:
     all_teams.difference_update(team_6)
-    y=  ("The second pick goes to ", team6[0])
+    y=  ("The second pick goes to "+ team6[0])
 elif p2 in team_7:
     all_teams.difference_update(team_7)
-    y=  ("The second pick goes to ", team7[0])
+    y=  ("The second pick goes to "+ team7[0])
 elif p2 in team_8:
     all_teams.difference_update(team_8)
-    y=  ("The second pick goes to ", team8[0])
+    y=  ("The second pick goes to "+ team8[0])
 elif p2 in team_9:
     all_teams.difference_update(team_9)
-    y=  ("The second pick goes to ", team9[0])
+    y=  ("The second pick goes to "+ team9[0])
 elif p2 in team_10:
     all_teams.difference_update(team_10)
-    y=  ("The second pick goes to ", team10[0])
+    y=  ("The second pick goes to "+ team10[0])
 elif p2 in team_11:
     all_teams.difference_update(team_11)
-    y=  ("The second pick goes to ", team11[0])
+    y=  ("The second pick goes to "+ team11[0])
 else:
     all_teams.difference_update(team_12)
-    y =  ("The second pick goes to ", team12[0])
-
-print (all_teams)
+    y =  ("The second pick goes to "+ team12[0])
 
 
 ## Third pick
 thirdPick = random.sample(all_teams, 1)
 p3 = thirdPick[0]
-print (p3)
 
 if p3  in team_1:
     all_teams.difference_update(team_1)
-    z =  ("The third pick goes to ", team1[0])
+    z =  ("The third pick goes to "+ team1[0])
 elif p3 in team_2:
     all_teams.difference_update(team_2)
-    z=  ("The third  pick goes to ", team2[0])
+    z=  ("The third  pick goes to "+ team2[0])
 elif p3 in team_3:
     all_teams.difference_update(team_3)
-    z=  ("The third pick goes to ", team3[0])
+    z=  ("The third pick goes to "+ team3[0])
 elif p3 in team_4:
     all_teams.difference_update(team_4)
-    z=  ("The third pick goes to ", team4[0])
+    z=  ("The third pick goes to "+ team4[0])
 elif p3 in team_5:
     all_teams.difference_update(team_5)
-    z=  ("The third pick goes to ", team5[0])
+    z=  ("The third pick goes to "+ team5[0])
 elif p3 in team_6:
     all_teams.difference_update(team_6)
-    z=  ("The third pick goes to ", team6[0])
+    z=  ("The third pick goes to "+ team6[0])
 elif p3 in team_7:
     all_teams.difference_update(team_7)
-    z=  ("The third pick goes to ", team7[0])
+    z=  ("The third pick goes to "+ team7[0])
 elif p3 in team_8:
     all_teams.difference_update(team_8)
-    z=  ("The third pick goes to ", team8[0])
+    z=  ("The third pick goes to "+ team8[0])
 elif p3 in team_9:
     all_teams.difference_update(team_9)
-    z=  ("The third pick goes to ", team9[0])
+    z=  ("The third pick goes to "+ team9[0])
 elif p3 in team_10:
     all_teams.difference_update(team_10)
-    z=  ("The third pick goes to ", team10[0])
+    z=  ("The third pick goes to "+ team10[0])
 elif p3 in team_11:
     all_teams.difference_update(team_11)
-    z=  ("The third pick goes to ", team11[0])
+    z=  ("The third pick goes to "+ team11[0])
 else:
     all_teams.difference_update(team_12)
-    z =  ("The third pick goes to ", team12[0])
+    z =  ("The third pick goes to "+ team12[0])
 
-print (all_teams)
 
 time.sleep(2)
-print (z)
+print ("The third number selected was the number " +str(p3) + ". " + z + ".")
 time.sleep(5)
-print (y)
+print()
+print ("The second number selected was the number " + str(p2) + ". " + y + ".")
 time.sleep(10)
-print (x)
+print()
+print ("The first number selected was the number " + str(p3) + ". " + x + ". Congratulations!")
